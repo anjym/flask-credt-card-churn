63a4f80cc704bbee9f58fa1f00fd14649c0b037a
anjym <anjymmichael123@gmail.com>
2021-08-15 01:06:19 +0000

    First commit

---
+++ app.py
@@ -0,0 +1,136 @@
+from flask import Flask,render_template,url_for,request,jsonify
+from flask_table import Table, Col
+from flask_sqlalchemy import SQLAlchemy
+
+import os
+import pickle
+import pandas as pd
+import numpy as np
+import sqlite3
+
+#Init App
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite3:///churnclient2.sqlite3'
+db=SQLAlchemy(app)
+# ENV ='dev'
+ 
+
+# if ENV =='dev':
+# 	app.debug=True
+# 	app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite3:///churnclient2.sqlite3'
+# else:
+# 	app.debug=False
+#  	SECRET_KEY=bkhvgyskhfjbkhqgkkshjvbkjgkbvbsgghbvnbvjvghjgbvjvgf
+#  	DATABASE_URL=sqlite:///churnclient2.sqlite3
+# 	app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+#  	SECRET_KEY = os.environ.get('SECRET_KEY')
+# 	app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+
+# 	db=SQLAlchemy(app)
+
+# class Dataweb1(db.Model):
+# 	__tablename__='dataweb1'
+# 	CLIENTNUM = db.column(db.Integer,primary_key=True)
+# 	Total_Trans_Ct = db.column(db.Integer)
+# 	Total_Revolving_Bal = db.column(db.Integer)
+# 	Total_Relationship_Count = db.column(db.Integer)
+# 	Total_Ct_Chng_Q4_Q1 = db.column(db.Float)
+# 	Total_Trans_Amt = db.column(db.Integer)
+# 	Months_Inactive_12_mon = db.column(db.Integer)
+# 	Total_Amt_Chng_Q4_Q1 = db.column(db.Integer)
+# 	Attrition_Flag = db.column(db.Text())
+
+# 	def __init__(Self,CLIENTNUM,Total_Trans_Ct,Total_Revolving_Bal,Total_Relationship_Count,Total_Ct_Chng_Q4_Q1,Total_Trans_Amt,Months_Inactive_12_mon,Total_Amt_Chng_Q4_Q1,Attrition_Flag):
+# 		self.CLIENTNUM = CLIENTNUM
+# 		self.Total_Trans_Ct = Total_Trans_Ct
+# 		self.Total_Revolving_Bal = Total_Revolving_Bal
+# 		self.Total_Relationship_Count = Total_Relationship_Count
+# 		self.Total_Ct_Chng_Q4_Q1 = Total_Ct_Chng_Q4_Q1
+# 		self.Total_Trans_Amt = Total_Trans_Amt
+# 		self.Months_Inactive_12_mon = Months_Inactive_12_mon
+# 		self.Total_Amt_Chng_Q4_Q1 = Total_Amt_Chng_Q4_Q1
+# 		self.Attrition_Flag = Attrition_Flag
+
+
+
+
+@app.route('/')
+def index():
+	return render_template('index.html ')
+
+@app.route('/test')
+def test():
+	return render_template('test.html ')	
+
+
+
+@app.route('/predictun')
+def predictun():
+	return render_template('predictuniq.html ')	
+
+
+@app.route('/predictplus')
+def predictplus():
+	return render_template('predictmul.html')
+
+
+# @app.route("/predict_un")
+# def predict_un():
+	
+
+@app.route('/predict_un',methods=['GET','POST'])
+def predict_un():
+	#loadind model
+	pickle_in = open('models/model.pkl', 'rb')
+	clf = pickle.load(pickle_in)
+	
+
+	if request.method == 'POST':
+		namequery1 = request.form['namequery1']
+		namequery2 = request.form['namequery2']
+		namequery3 = request.form['namequery3']
+		namequery4 = request.form['namequery4']
+		namequery5 = request.form['namequery5']
+		namequery6 = request.form['namequery6']
+		namequery7 = request.form['namequery7']
+				
+		result = clf.predict(np.array([[namequery1, namequery3, namequery3, namequery4,namequery5,namequery6,namequery7]]).astype(np.float64))
+		if result==1:
+			res="Prediction : Client désouscrit ou  ayant une probabilité de Supérieur a 99 pourcent de départ."
+		elif result==0:
+			#print(res)
+			res="Prediction : Client Existant sur le Service"
+			#print(res)
+		return render_template('predictuniq.html', prediction=res)
+
+
+@app.route('/pred_plus',methods=['GET','POST'])
+def pred_plus():
+	if request.method == 'POST':
+		client_numb=int(request.form.get('client_numb'))
+		conn=sqlite3.connect('churnclient2.sqlite3')
+		cur=conn.cursor()
+		cur.execute('SELECT CLIENTNUM,Total_Trans_Ct,Total_Revolving_Bal,Total_Relationship_Count,Total_Ct_Chng_Q4_Q1,Total_Trans_Amt,Months_Inactive_12_mon,Total_Amt_Chng_Q4_Q1,Attrition_Flag FROM `dataweb1`ORDER BY RANDOM()LIMIT {}'.format(client_numb))
+		datadb=cur.fetchall()
+		cur.close()
+		dat=np.array(datadb)
+		df1=pd.DataFrame(dat, columns=['CLIENTNUM','Total_Trans_Ct','Total_Revolving_Bal','Total_Relationship_Count','Total_Ct_Chng_Q4_Q1','Total_Trans_Amt','Months_Inactive_12_mon','Total_Amt_Chng_Q4_Q1','Attrition_Flag'])
+		indexs=df1['CLIENTNUM']
+		df_x2= df1.drop(['CLIENTNUM','Attrition_Flag'],axis=1)
+
+		#loadind model
+		pickle_in = open('models/model.pkl', 'rb')
+		clf = pickle.load(pickle_in)
+		result = clf.predict(np.array(df_x2).astype(np.float64))
+
+		#print(result)
+		resul2=pd.DataFrame(result,columns=['prediction'])
+		resul2=resul2.replace({1: "Attrited Customer",0:"Existant sur le Service"})
+		df2=pd.concat([df1,resul2],axis=1)
+		df2.columns = ['Identifiant Client','Nbre de Transactions','Solde renouvelable de la carte','Nombre de produits détenus','Changement du nbre de transactions','Montant de transaction','Nbre Mois Inactif','Montant des transaction Q4-Q1','Staut_Actuel','prediction']
+		heat=['Identifiant Client','Nbre de Transactions','Solde renouvelable de la carte','Nombre de produits détenus','Changement du nbre de transactions','Montant de transaction','Nbre Mois Inactif','Montant des transaction Q4-Q1','Staut_Actuel','prediction']
+		val=df2[heat].values
+	return render_template('t.html', heat=heat, val=val)
+
+if __name__=='main__':
+	app.run()
---
+++ templates/test.php
@@ -0,0 +1,237 @@
+<?php
+$tableau = tables
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+  	<title></title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,700' rel='stylesheet' type='text/css'>
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+	<link rel="stylesheet" href="../static/css/style.css">
+
+	</head>
+	<body>
+	<section class="ftco-section">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-6 text-center mb-5">
+					<h2 class="heading-section">Prédiction de Churn Client du Service Credit_Card</h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12">
+					<div class="table-wrap">
+						<table class="table table-responsive-xl" id="newTable">
+						  <thead class="thead-dark">
+						    <tr>
+						    	<th>&nbsp;</th>
+						    	<th>CLIENTNUM</th>
+						      <th>Total_Trans_Ct</th>
+						      <th>Total_Revolving_Bal</th>
+						      <th>Total_Relationship_Count</th>
+						      <th>Total_Ct_Chng_Q4_Q1</th>
+						      <th>Total_Trans_Amt</th>
+						      <th>Months_Inactive_12_mon</th>
+						      <th>Total_Amt_Chng_Q4_Q1</th>
+						      <th>Attrition_Flag</th>
+						      <th>Prédiction</th>
+						      <th>&nbsp;</th>
+						    </tr>
+						  </thead>
+						  <!-- <tbody>
+						    <tr class="alert" role="alert">
+						    	<td>
+						    		<label class="checkbox-wrap checkbox-primary">
+										  <input type="checkbox" checked>
+										  <span class="checkmark"></span>
+										</label>
+						    	</td>
+						      <td class="d-flex align-items-center">
+						      	<div class="img" style="background-image: url(images/person_1.jpg);"></div>
+						      	<div class="pl-3 email">
+						      		<span>markotto@email.com</span>
+						      		<span>Added: 01/03/2020</span>
+						      	</div>
+						      </td>
+						      <td>Markotto89</td>
+						      <td class="status"><span class="active">Active</span></td>
+						      <td>
+						      	<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+				            	<span aria-hidden="true"><i class="fa fa-close"></i></span>
+				          	</button>
+				        	</td>
+						    </tr>
+						    <tr class="alert" role="alert">
+						    	<td>
+						    		<label class="checkbox-wrap checkbox-primary">
+										  <input type="checkbox">
+										  <span class="checkmark"></span>
+										</label>
+						    	</td>
+						      <td class="d-flex align-items-center">
+						      	<div class="img" style="background-image: url(images/person_2.jpg);"></div>
+						      	<div class="pl-3 email">
+						      		<span>jacobthornton@email.com</span>
+						      		<span>Added: 01/03/2020</span>
+						      	</div>
+						      </td>
+						      <td>Jacobthornton</td>
+						      <td class="status"><span class="waiting">Waiting for Resassignment</span></td>
+						      <td>
+						      	<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+				            	<span aria-hidden="true"><i class="fa fa-close"></i></span>
+				          	</button>
+				        	</td>
+						    </tr>
+						    <tr class="alert" role="alert">
+						    	<td>
+						    		<label class="checkbox-wrap checkbox-primary">
+										  <input type="checkbox">
+										  <span class="checkmark"></span>
+										</label>
+						    	</td>
+						      <td class="d-flex align-items-center">
+						      	<div class="img" style="background-image: url(images/person_3.jpg);"></div>
+						      	<div class="pl-3 email">
+						      		<span>larrybird@email.com</span>
+						      		<span>Added: 01/03/2020</span>
+						      	</div>
+						      </td>
+						      <td>Larry_bird</td>
+						      <td class="status"><span class="active">Active</span></td>
+						      <td>
+						      	<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+				            	<span aria-hidden="true"><i class="fa fa-close"></i></span>
+				          	</button>
+				        	</td>
+						    </tr>
+						    <tr class="alert" role="alert">
+						    	<td>
+						    		<label class="checkbox-wrap checkbox-primary">
+										  <input type="checkbox">
+										  <span class="checkmark"></span>
+										</label>
+						    	</td>
+						      <td class="d-flex align-items-center">
+						      	<div class="img" style="background-image: url(images/person_4.jpg);"></div>
+						      	<div class="pl-3 email">
+						      		<span>johndoe@email.com</span>
+						      		<span>Added: 01/03/2020</span>
+						      	</div>
+						      </td>
+						      <td>Johndoe1990</td>
+						      <td class="status"><span class="active">Active</span></td>
+						      <td>
+						      	<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+				            	<span aria-hidden="true"><i class="fa fa-close"></i></span>
+				          	</button>
+				        	</td>
+						    </tr>
+						    <tr class="alert" role="alert">
+						    	<td class="border-bottom-0">
+						    		<label class="checkbox-wrap checkbox-primary">
+										  <input type="checkbox">
+										  <span class="checkmark"></span>
+										</label>
+						    	</td>
+						      <td class="d-flex align-items-center border-bottom-0">
+						      	<div class="img" style="background-image: url(images/person_1.jpg);"></div>
+						      	<div class="pl-3 email">
+						      		<span>garybird@email.com</span>
+						      		<span>Added: 01/03/2020</span>
+						      	</div>
+						      </td>
+						      <td class="border-bottom-0">Garybird_2020</td>
+						      <td class="status border-bottom-0"><span class="waiting">Waiting for Resassignment</span></td>
+						      <td class="border-bottom-0">
+						      	<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+				            	<span aria-hidden="true"><i class="fa fa-close"></i></span>
+				          	</button>
+				        	</td>
+						    </tr>
+						  </tbody> -->
+						</table>
+                        
+                        
+                        {% for table in tables %}
+            
+           					 {{ table|safe }}
+						{% endfor %}
+
+
+						<br><br>
+                        <button onclick="exportTableToCSV('client.csv')">Télécharger(Format CSV)</button><br>	
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<script src="../static/js/jquery.min.js"></script>
+  <script src="../static/js/popper.js"></script>
+  <script src="../static/js/bootstrap.min.js"></script>
+  <script src="../static/js/main.js"></script>
+
+
+  
+  <script type="text/javascript">
+
+function downloadCSV(csv, filename) {
+    var csvFile;
+    var downloadLink;
+
+    // CSV file
+    csvFile = new Blob([csv], {type: "text/csv"});
+
+    // Download link
+    downloadLink = document.createElement("a");
+
+    // File name
+    downloadLink.download = filename;
+
+    // Create a link to the file
+    downloadLink.href = window.URL.createObjectURL(csvFile);
+
+    // Hide download link
+    downloadLink.style.display = "none";
+
+    // Add the link to DOM
+    document.body.appendChild(downloadLink);
+
+    // Click download link
+    downloadLink.click();
+}
+
+
+
+
+
+function exportTableToCSV(filename) {
+    var csv = [];
+    var rows = document.querySelectorAll("table tr");
+    
+    for (var i = 0; i < rows.length; i++) {
+        var row = [], cols = rows[i].querySelectorAll("td, th");
+        
+        for (var j = 0; j < cols.length; j++) 
+            row.push(cols[j].innerText);
+        
+        csv.push(row.join(","));        
+    }
+
+    // Download CSV file
+    downloadCSV(csv.join("\n"), filename);
+}
+</script>
+
+
+	</body>
+</html>
+
